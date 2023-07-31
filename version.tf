e91a1054e27a23eaafe75931ea163bc666bd5f8f
Vickey-Wu <1143604452@qq.com>
2023-07-31 23:26:19 +0800

    feat: add provider

---
+++ version.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = "1.4.2"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.10.0"
+    }
+
+    alicloud = {
+      source = "aliyun/alicloud"
+      version = "1.208.1"
+    }
+  }
+}
+
+provider "aws" {
+  # access_key = "my-access-key"
+  # secret_key = "my-secret-key"
+  region = "ap-east-1"
+}
+
+provider "alicloud" {
+  # access_key = "my-access-key"
+  # secret_key = "my-secret-key"
+  region = "cn-hangzhou-1"
+}
