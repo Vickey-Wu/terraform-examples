40ff66a4aba44b75d726d22ef3d07ea648a29231
Vickey-Wu <1143604452@qq.com>
2023-08-08 20:39:43 +0800

    feat: add eks cluster

---
+++ eks-cluster.tf
@@ -0,0 +1,42 @@
+resource "aws_iam_role" "eks-iam-role" {
+  name = "eks-iam-role"
+
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+  {
+   "Effect": "Allow",
+   "Principal": {
+    "Service": "eks.amazonaws.com"
+   },
+   "Action": "sts:AssumeRole"
+  }
+ ]
+}
+EOF
+
+}
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks-iam-role.name
+}
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly-EKS" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks-iam-role.name
+}
+
+resource "aws_eks_cluster" "eks-prod" {
+  name     = "eks-prod"
+  role_arn = aws_iam_role.eks-iam-role.arn
+
+  vpc_config {
+    subnet_ids = [var.subnet_id_1, var.subnet_id_2]
+  }
+
+  depends_on = [
+    aws_iam_role.eks-iam-role,
+  ]
+}
