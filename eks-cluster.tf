1c8c4a3747d56bc4e20bb1989e103cd5cdcf138f
Vickey-Wu <1143604452@qq.com>
2023-08-10 20:25:21 +0800

    feat: eks cluster add oidc and add-on

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -43,3 +43,47 @@ resource "aws_eks_cluster" "bcp" {
   #  aws_iam_role.eks-cluster-role,
   #]
 }
+
+## enalbe oidc
+data "tls_certificate" "tls_cert" {
+  url = aws_eks_cluster.bcp.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "oidc" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = data.tls_certificate.tls_cert.certificates[*].sha1_fingerprint
+  url             = data.tls_certificate.tls_cert.url
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.oidc.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:aws-node"]
+    }
+
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.oidc.arn]
+      type        = "Federated"
+    }
+  }
+}
+
+#resource "aws_iam_role" "example" {
+#  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+#  name               = "example"
+#}
+
+## eks addon
+#resource "aws_eks_addon" "aws-ebs-csi-driver" {
+#  cluster_name = aws_eks_cluster.bcp.name
+#  addon_name   = "aws-ebs-csi-driver"
+#}
+#resource "aws_eks_addon" "aws-efs-csi-driver" {
+#  cluster_name = aws_eks_cluster.bcp.name
+#  addon_name   = "aws-efs-csi-driver"
+#}
